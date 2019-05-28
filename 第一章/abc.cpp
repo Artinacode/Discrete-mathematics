eb61ce89e82b1191b1779900af2e4bda3b5c273e
80781650 <80781650@qq.com>
2019-05-28 11:07:35 +0800

    111

---
+++ 第一章/abc.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 齐国伟 on 2019/5/28.
+//
+
+#include "abc.h"
---
+++ 第一章/abc.h
@@ -0,0 +1,14 @@
+//
+// Created by 齐国伟 on 2019/5/28.
+//
+
+#ifndef DISCRETE_MATHEMATICS_ABC_H
+#define DISCRETE_MATHEMATICS_ABC_H
+
+
+class abc {
+
+};
+
+
+#endif //DISCRETE_MATHEMATICS_ABC_H
